f2d5f49cefa03119cb845a04f06f4d2aef0e994c
Isaias Cuvula <68303716+IsaiasCuvula@users.noreply.github.com>
2023-08-07 01:41:09 +0300

    separated folder for task provider and notifier

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,6 +1,4 @@
 export 'category_provider.dart';
-export 'tasks_provider.dart';
 export 'date_provider.dart';
 export 'time_provider.dart';
-export 'task_notifier.dart';
-export 'task_state.dart';
+export 'task/task.dart';
---
+++ lib/providers/task/task.dart
@@ -0,0 +1,3 @@
+export 'task_notifier.dart';
+export 'task_state.dart';
+export 'tasks_provider.dart';
